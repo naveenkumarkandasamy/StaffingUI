0132e0e59e3280ba1c90d3a0db04cec3a7af879d
payal <payal.mantri@accoliteindia.com>
2019-12-26 20:03:14 +0530

    Add: Connect to backend to validate the user login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule  }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { AgGridModule } from 'ag-grid-angular';
-import { MatToolbarModule, MatIconModule, MatSidenavModule,  MatButtonModule, MatCheckboxModule} from "@angular/material";
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatButtonModule, MatCheckboxModule } from "@angular/material";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphComponent } from './graph/graph.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MainFormComponent } from './form/form.component';
 import { AutorunComponent } from "./autorun/autorun.component"
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 //Services
 import { DataService } from "./services/data.service";
-import {ConstantsService} from "./services/constants.service";
+import { ConstantsService } from "./services/constants.service";
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { AuthenticationService } from './services/authentication.service';
+
 
 
 
@@ -31,7 +33,7 @@ import { LogoutComponent } from './logout/logout.component';
     AutorunComponent,
     LoginComponent,
     LogoutComponent,
-  
+
   ],
   imports: [
     MatGridListModule,
@@ -39,7 +41,7 @@ import { LogoutComponent } from './logout/logout.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule     ,
+    FormsModule,
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
@@ -48,7 +50,7 @@ import { LogoutComponent } from './logout/logout.component';
     AgGridModule.withComponents([]),
     ToastrModule.forRoot(),
   ],
-  providers: [DataService, ConstantsService],
+  providers: [DataService, ConstantsService, AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
