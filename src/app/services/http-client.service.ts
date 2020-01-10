db79061ea114c53a306206045a1d4a24692d275a
payal <payal.mantri@accoliteindia.com>
2020-01-10 17:40:05 +0530

    Add: Environment Changes

--- src/app/services/http-client.service.ts
+++ src/app/services/http-client.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { response, Model } from "../Models/app.types"
+import { environment } from "../../environments/environment"
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
 
-  constructor(private http: HttpClient) { }
+  private apiUrl:string="";
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiUrl;
+   }
 
   getGraphDetailsUsingTableData(requestBody) {
-    const apiLink = '/Staffing/api/request/shiftPlan';
+    console.log(environment.environment)
+    const apiLink = this.apiUrl+'/Staffing/api/request/shiftPlan';
     return this.http.post<response>(apiLink, requestBody);
   }
 
   getGraphDetailsUsingFileData(requestBody) {
-    const apiLink = '/Staffing/api/request/shiftPlanFileUpload';
+    const apiLink = this.apiUrl+'/Staffing/api/request/shiftPlanFileUpload';
     return this.http.post<response>(apiLink, requestBody);
   }
 }
